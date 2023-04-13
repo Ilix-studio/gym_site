22c4cbbf4dfeecf829f8b2c96ba61c1050686f62
Ilix-studio <ilishjyoti17@gmail.com>
2023-04-13 17:03:35 +0530

    something done

--- main.js
+++ main.js
@@ -35,49 +35,70 @@ const scrollHeader = () => {
 };
 window.addEventListener("scroll", scrollHeader);
 
-// calculate JS 
-const calculateForm = document.getElementById('calculate-form'),
-      calculateCm = document.getElementById('calculate-cm'),
-      calculateKg = document.getElementById('calculate-kg'),
-      calculateMessage = document.getElementById('calculate-message')
+// calculate JS
+const calculateForm = document.getElementById("calculate-form"),
+  calculateCm = document.getElementById("calculate-cm"),
+  calculateKg = document.getElementById("calculate-kg"),
+  calculateMessage = document.getElementById("calculate-message");
 
 const calculateBmi = (event) => {
- event.preventDefault()
-//  check if the field have any value 
-if(calculateCm.value === "" || calculateKg.value === ""){
-  //add and remove color
-  calculateMessage.classList.remove("color-green")
-  calculateMessage.classList.add("color-red")
-  //show message 
-  calculateMessage.textContent = "Fill in the height and weight"
-  //remove message after three sec
-  setTimeout(() => {
-    calculateMessage.textContent = ""
-  }, 3000)
-}else{
-  // calculate the BMI formula 
-  const cm = calculate.value / 100,
-        kg = calculateKg.value,
-        bmi = Math.round(kg / (cm * cm))
-        //show the health status
-        if(bmi < 18.5){
-          // show the message with color 
-          calculateMessage.classList.add("color-green")
-          calculateMessage.textContent = "your BMI is ${bmi} and you are skinny "
-        }else if(bmi < 25){
-          calculateMessage.classList.add("color-green")
-          calculateMessage.textContent = "your BMI is ${bmi} and you are healthy"
-        }else{
-          calculateMessage.classList.add("color-green")
-          calculateMessage.textContent = "your BMI is ${bmi} and you are over-weight"
-        }
-        //clear all
-        calculateCm.value = ""
-        calculateKg.vlaue = ""
+  event.preventDefault();
+  //  check if the field have any value
+  if (calculateCm.value === "" || calculateKg.value === "") {
+    //add and remove color
+    calculateMessage.classList.remove("color-green");
+    calculateMessage.classList.add("color-red");
+    //show message
+    calculateMessage.textContent = "Fill in the height and weight";
+    //remove message after three sec
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 3000);
+  } else {
+    // calculate the BMI formula
+    const cm = calculate.value / 100,
+      kg = calculateKg.value,
+      bmi = Math.round(kg / (cm * cm));
+    //show the health status
+    if (bmi < 18.5) {
+      // show the message with color
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = "your BMI is ${bmi} and you are skinny ";
+    } else if (bmi < 25) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = "your BMI is ${bmi} and you are healthy";
+    } else {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent =
+        "your BMI is ${bmi} and you are over-weight";
+    }
+    //clear all
+    calculateCm.value = "";
+    calculateKg.vlaue = "";
 
-        setTimeout(() => {
-          calculateMessage.textContent = ""
-        }, 4000)
-}
-}
-calculateForm.addEventListener('submit', calculateBmi)
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 4000);
+  }
+};
+calculateForm.addEventListener("submit", calculateBmi);
+
+// Scroll Sections Active Link
+const sections = document.querySelectorAll("section[id]");
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = curent.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.add("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
